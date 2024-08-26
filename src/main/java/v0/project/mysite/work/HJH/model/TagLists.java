6b5873aabc9560bc86a4edbf828d4146f8148d79
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-08-26 19:21:12 +0900

    tagLists 매핑

---
+++ src/main/java/v0/project/mysite/work/HJH/model/TagLists.java
@@ -0,0 +1,13 @@
+package v0.project.mysite.work.HJH.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter @Setter
+public class TagLists {
+    private int tag_id;
+    private int announcement_id;
+    private int tag_filter_id;
+}
