7fafbf1a35b7c3bbf72db38a438eeb6a175f0b26
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2024-08-28 12:27:54 +0900

    지원자 리스트용 주소 매핑

---
+++ src/main/java/v0/project/mysite/work/HJH/model/ViewBizMembersApplies.java
@@ -0,0 +1,23 @@
+package v0.project.mysite.work.HJH.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Data
+@Getter @Setter
+public class ViewBizMembersApplies {
+    private String announcement;
+    private String nick_name;
+    private String image_url;
+    private String userprofile;
+
+    private int apply_id;
+    private int member_id;
+    private int announcement_id;
+    private int chat_created;
+    private int user_hired;
+    private Timestamp apply_date;
+}
