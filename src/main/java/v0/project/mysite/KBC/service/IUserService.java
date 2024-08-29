64569933e6fcda05efd7c3b6f3bee3183e241f43
Beomky <nobleman1214@naver.com>
2024-08-29 17:05:05 +0900

    Create:resume api

--- src/main/java/v0/project/mysite/KBC/service/IUserService.java
+++ src/main/java/v0/project/mysite/KBC/service/IUserService.java
@@ -3,7 +3,11 @@ package v0.project.mysite.KBC.service;
 import org.springframework.security.core.Authentication;
 import v0.project.mysite.KBC.DTO.TestBiz;
 import v0.project.mysite.work.HJH.model.BizMembers;
+import v0.project.mysite.work.HJH.model.Members;
+
+import java.util.List;
 
 public interface IUserService {
-   TestBiz getBizMembers(Authentication authentication);
+   BizMembers getBizMembers(Authentication authentication);
+   List<Members> selectAll();
 }
